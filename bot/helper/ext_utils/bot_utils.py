7cb4c21f9c947cd50ddfbb0735e13029484bc225
mohit828 <145571381+mohit828@users.noreply.github.com>
2023-11-03 14:16:53 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -157,7 +157,7 @@ def source(self):
 
 
 def get_readable_message():
-    msg = '<b>Powered by Aeon</b>\n\n'
+    msg = '<a href=https://telegram.me/zenupdatess>Pᴏᴡᴇʀᴇᴅ Bʏ Zᴇɴ Uᴘᴅᴀᴛᴇs</a>\n\n'
     button = None
     tasks = len(download_dict)
     currentTime = get_readable_time(time() - botStartTime)
